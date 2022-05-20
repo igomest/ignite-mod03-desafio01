1d6e1aba8ba97c45c686fea63089008323f4e086
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-20 10:07:06 -0300

    styling home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+import styles from './header.module.scss'
+import Image from 'next/image'
+
 export default function Header() {
-  // TODO
+  // return (
+  //   <header>
+  //     <div>
+  //       <Image
+  //         priority
+  //         src="/images/Logo.svg"
+  //         alt="Logo"
+  //         height={238.62}
+  //         width={25.63}
+  //       />
+  //     </div>
+  //   </header>
+  // )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 interface Post {
   uid?: string;
@@ -25,7 +28,49 @@ interface HomeProps {
 }
 
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>spacetraveling | Home</title>
+      </Head>
+
+      <div className={styles.postListContainer}>
+        <ul>
+          <li>
+            <Image
+              priority
+              src="/images/Logo.svg"
+              alt="Logo"
+              height={25.63}
+              width={238.62}
+            />
+          </li>
+
+          <li>
+            <div className={styles.listContainer}>
+              <h3>Como utilizar Hooks</h3>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.iconContainer}>
+                <div className={styles.icon}>
+                  <FiCalendar />
+                  <p>15 Mar 2021</p>
+
+                  <FiUser />
+                  <p>Klein Moretti</p>
+                </div>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <button type="button">
+              <p>Carregar mais posts</p>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 // export const getStaticProps = async () => {
